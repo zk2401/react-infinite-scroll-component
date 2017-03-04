a281c8dc25af0e1382c49f91870de399a9d30c4b
Ankeet Maini <ankeet.maini@gmail.com>
2017-03-05 01:38:32 +0530

    Changes demo div styles (#13)

--- demos/height.js
+++ demos/height.js
@@ -3,22 +3,25 @@ import ReactDOM from 'react-dom';
 
 import InfiniteScroll from '../app';
 
+const style = {
+  display: 'flex',
+  alignItems: 'center',
+  fontSize: 40
+};
+
 const divs = [
-  <div key={1} style={{height: 200, background: 'cornsilk'}}>Big div no 1</div>,
-  <div key={2} style={{height: 200, background: 'cornsilk'}}>Big div no 2</div>,
-  <div key={3} style={{height: 200, background: 'cornsilk'}}>Big div no 3</div>,
-  <div key={4} style={{height: 200, background: 'cornsilk'}}>Big div no 4</div>,
-  <div key={5} style={{height: 200, background: 'cornsilk'}}>Big div no 5</div>,
-  <div key={6} style={{height: 200, background: 'cornsilk'}}>Big div no 6</div>,
-  <div key={7} style={{height: 200, background: 'cornsilk'}}>Big div no 7</div>,
-  <div key={8} style={{height: 200, background: 'cornsilk'}}>Big div no 8</div>,
-  <div key={9} style={{height: 200, background: 'cornsilk'}}>Big div no 9</div>,
-  <div key={10} style={{height: 200, background: 'cornsilk'}}>Big div no 10</div>,
-  <div key={11} style={{height: 200, background: 'cornsilk'}}>Big div no 11</div>,
-  <div key={12} style={{height: 200, background: 'cornsilk'}}>Big div no 12</div>,
+  <div key={1} style={{height: 200, background: '#9bc95b', ...style}}>Div no 1</div>,
+  <div key={2} style={{height: 200, background: '#ffd47b', ...style}}>Div no 2</div>,
+  <div key={3} style={{height: 200, background: '#95a9d6', ...style}}>Div no 3</div>,
+  <div key={4} style={{height: 200, background: '#ffa8e1', ...style}}>Div no 4</div>,
+  <div key={5} style={{height: 200, background: '#9bc95b', ...style}}>Div no 5</div>,
+  <div key={6} style={{height: 200, background: '#ffd47b', ...style}}>Div no 6</div>,
+  <div key={7} style={{height: 200, background: '#95a9d6', ...style}}>Div no 7</div>,
+  <div key={8} style={{height: 200, background: '#ffa8e1', ...style}}>Div no 8</div>,
 ];
 
 const heightMessage = 'Infinite Scroll given fixed height of 300px in props';
+const colors = ['#9bc95b', '#ffd47b', '#95a9d6', '#ffa8e1'];
 
 export default class Height extends React.Component {
   constructor () {
@@ -32,7 +35,7 @@ export default class Height extends React.Component {
     let count = this.state.divs.length;
     for (let i = 0; i < 30; i++) {
       moreDivs.push(
-        <div key={'div' + count++} style={{background: 'cornsilk', height: 100}}>
+        <div key={'div' + count++} style={{height: 200, background: colors[i % 4], ...style}}>
           Div no {count}
         </div>
       );
--- demos/no-height.js
+++ demos/no-height.js
@@ -3,19 +3,25 @@ import ReactDOM from 'react-dom';
 
 import InfiniteScroll from '../app';
 
+const style = {
+  display: 'flex',
+  alignItems: 'center',
+  fontSize: 40
+};
+
 const divs = [
-  <div key={1} style={{height: 250, background: 'cornsilk'}}>Big div no 1</div>,
-  <div key={2} style={{height: 250, background: 'cornsilk'}}>Big div no 2</div>,
-  <div key={3} style={{height: 250, background: 'cornsilk'}}>Big div no 3</div>,
-  <div key={4} style={{height: 250, background: 'cornsilk'}}>Big div no 4</div>,
-  <div key={5} style={{height: 250, background: 'cornsilk'}}>Big div no 5</div>,
-  <div key={6} style={{height: 250, background: 'cornsilk'}}>Big div no 6</div>,
-  <div key={7} style={{height: 250, background: 'cornsilk'}}>Big div no 7</div>,
-  <div key={8} style={{height: 250, background: 'cornsilk'}}>Big div no 8</div>,
-  <div key={9} style={{height: 250, background: 'cornsilk'}}>Big div no 9</div>,
+  <div key={1} style={{height: 200, background: '#9bc95b', ...style}}>Div no 1</div>,
+  <div key={2} style={{height: 200, background: '#ffd47b', ...style}}>Div no 2</div>,
+  <div key={3} style={{height: 200, background: '#95a9d6', ...style}}>Div no 3</div>,
+  <div key={4} style={{height: 200, background: '#ffa8e1', ...style}}>Div no 4</div>,
+  <div key={5} style={{height: 200, background: '#9bc95b', ...style}}>Div no 5</div>,
+  <div key={6} style={{height: 200, background: '#ffd47b', ...style}}>Div no 6</div>,
+  <div key={7} style={{height: 200, background: '#95a9d6', ...style}}>Div no 7</div>,
+  <div key={8} style={{height: 200, background: '#ffa8e1', ...style}}>Div no 8</div>,
 ];
 
 const noHeightMessage = 'No height given to InfiniteScroll, free scroll like Facebook. Also try Pull Down to refresh! :P';
+const colors = ['#9bc95b', '#ffd47b', '#95a9d6', '#ffa8e1'];
 
 export default class NoHeight extends React.Component {
   constructor () {
@@ -32,7 +38,7 @@ export default class NoHeight extends React.Component {
     let count = this.state.divs.length;
     for (let i = 0; i < 30; i++) {
       moreDivs.push(
-        <div key={'div' + count++} style={{background: 'cornsilk', height: 100}}>
+        <div key={'div' + count++} style={{height: 200, background: colors[i % 4], ...style}}>
           Div no {count}
         </div>
       );
